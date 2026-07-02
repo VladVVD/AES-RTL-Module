4631c0a6de295701110ab1c66de1c4789ed0cd3d
Vladyslav Dudarenko <vladdudarenko@ukr.net>
2026-07-03 00:29:05 +0200

    feat: add ShiftRows and MixColumns transformations

---
+++ aes_accelerator.srcs/sources_1/new/aes_shift_rows.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02.07.2026 23:37:56
+// Design Name: 
+// Module Name: aes_shift_rows
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module aes_shift_rows(
+
+    );
+endmodule
