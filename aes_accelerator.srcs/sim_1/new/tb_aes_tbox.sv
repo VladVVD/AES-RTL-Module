365066d291eb25a24754d83f331c92c49a8b36be
Vladyslav Dudarenko <vladdudarenko@ukr.net>
2026-07-02 23:29:53 +0200

    init: setup Vivado project and add working AES S-box

---
+++ aes_accelerator.srcs/sim_1/new/tb_aes_tbox.sv
@@ -0,0 +1,35 @@
+`timescale 1ns / 1ps
+
+module tb_aes_sbox();
+
+    logic [7:0] test_in;
+    logic [7:0] test_out;
+
+    aes_sbox uut (
+        .in_byte(test_in),
+        .out_byte(test_out)
+    );
+
+    initial begin
+        $display("Starting");
+
+        test_in = 8'h00;
+        #10;
+        if (test_out == 8'h63) $display("PASS: in=%h, out=%h", test_in, test_out);
+        else                   $display("FAIL: in=%h, out=%h (expected 63)", test_in, test_out);
+
+        test_in = 8'h01;
+        #10;
+        if (test_out == 8'h7c) $display("PASS: in=%h, out=%h", test_in, test_out);
+        else                   $display("FAIL: in=%h, out=%h (expected 7C)", test_in, test_out);
+
+        test_in = 8'hFF;
+        #10;
+        if (test_out == 8'h16) $display("PASS: in=%h, out=%h", test_in, test_out);
+        else                   $display("FAIL: in=%h, out=%h (expected 16)", test_in, test_out);
+
+        $display("Test finished.");
+        $finish;
+    end
+
+endmodule
---
+++ aes_accelerator.srcs/sources_1/new/aes_core.sv
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+
+module aes_core (
+    input  logic         clk,
+    input  logic         rst_n,
+
+
+    input  logic         start,
+    output logic         done,
+
+    input  logic [127:0] plaintext,
+    input  logic [127:0] key,
+    output logic [127:0] ciphertext
+);
+
+    always_ff @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            ciphertext <= 128'b0;
+            done       <= 1'b0;
+        end else if (start) begin
+
+            ciphertext <= plaintext ^ key;
+            done       <= 1'b1;
+        end else begin
+            done       <= 1'b0;
+        end
+    end
+
+endmodule
