e2a89bdcac95cb2c80e541e8f7c94326c508c11d
Vladyslav Dudarenko <vladdudarenko@ukr.net>
2026-07-03 13:02:36 +0200

    feat: implement AES-128 core FSM and FIPS-197 testbenches
    - Completed FSM and datapath integration in aes_core.sv
    - Finalized datapath wiring in aes_round.sv
    - Added testbenches tb_aes_round and tb_aes_core
    - Verified compliance with FIPS-197 Appendix B

--- aes_accelerator.srcs/sources_1/new/aes_core.sv
+++ aes_accelerator.srcs/sources_1/new/aes_core.sv
@@ -1,27 +1,100 @@
 `timescale 1ns / 1ps
 
 module aes_core (
-    input  logic clk,
-    input  logic rst_n,
+    input logic clk,
+    input logic rst,
 
-    input  logic start,
-    output logic done,
+    input logic start,
 
-    input  logic [127:0] plaintext,
-    input  logic [127:0] key,
-    output logic [127:0] ciphertext
+    input logic [127:0] plaintext_in,
+    input logic [127:0] key_in,
+    output logic [127:0] ciphertext_out,
+    output logic valid
 );
 
-    always_ff @(posedge clk or negedge rst_n) begin
-        if (!rst_n) begin
-            ciphertext <= 128'b0;
-            done <= 1'b0;
-        end else if (start) begin
-            ciphertext <= plaintext ^ key;
-            done <= 1'b1;
+    typedef enum logic [1:0] {INIT, CALC, DONE} state_t;
+    state_t state_p, state_n;
+    
+    logic [127:0] matrix_p, matrix_n;
+    logic [127:0] key_p, key_n;
+    logic [3:0] round_p, round_n;
+    logic valid_p, valid_n;
+    
+    logic [127:0] next_key;
+    logic [127:0] next_matrix;
+    logic last_round;
+
+    assign last_round = (round_p == 4'hA);
+
+    aes_key_expand key_inst (
+        .in_key(key_p),
+        .round_idx(round_p),
+        .out_key(next_key)
+    );
+
+    aes_round round_inst (
+        .state_in(matrix_p),
+        .round_key(next_key),
+        .last_round(last_round),
+        .state_out(next_matrix)
+    );
+
+    always_ff @(posedge clk or negedge rst) begin
+        if (!rst) begin
+            state_p <= INIT;
+            matrix_p <= 128'h0;
+            key_p <= 128'h0;
+            round_p <= 4'h0;
+            valid_p <= 1'b0;
         end else begin
-            done <= 1'b0;
+            state_p <= state_n;
+            matrix_p <= matrix_n;
+            key_p <= key_n;
+            round_p <= round_n;
+            valid_p <= valid_n;
         end
     end
-
+    
+    always_comb begin
+        state_n = state_p;
+        matrix_n = matrix_p;
+        key_n = key_p;
+        round_n = round_p;
+        valid_n = valid_p;
+        
+        case (state_p)
+            INIT: begin
+                valid_n = 1'b0;
+                
+                if (start) begin
+                    matrix_n = plaintext_in ^ key_in;
+                    key_n = key_in;
+                    round_n = 4'h1;
+                    state_n = CALC;
+                 end
+            end
+            
+            CALC: begin
+                matrix_n = next_matrix;
+                key_n = next_key;
+                
+                if (round_p == 4'ha) begin
+                    state_n = DONE;
+                end else begin
+                    round_n = round_p + 1;
+                end
+            end
+            DONE: begin
+                valid_n = 1'b1;
+                state_n = INIT;
+            end
+            default: begin
+                state_n = INIT;
+            end
+        endcase
+    end
+    
+    assign ciphertext_out = matrix_p;
+    assign valid = valid_p;
+    
 endmodule
